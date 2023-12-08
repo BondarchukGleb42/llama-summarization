73f45bf870a180d5a97ec5f824102df1aee803c0
Bondarchuk Gleb <72991231+BondarchukGleb42@users.noreply.github.com>
2023-12-08 23:26:45 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -49,13 +49,23 @@ def predict(dscr, pos_comms, neg_comms):
         )[0],
         skip_special_tokens=True
     )
-    output = output[output.index("### Плюсы и минусы:")+19:]
-    output = output[:output.index("###")+3]
-    output = output.split("\n\n")
-    
-    pluses = output[0].split("\n")[2:]
-    pluses = list(map(lambda x: x[x.index(")")+2:], pluses))
-    minuses = output[1].split("\n")[1:-1]
-    minuses = list(map(lambda x: x[x.index(")")+2:], minuses))
+    output = output[len(prompt):].lower()
+    if "плюсы" in output:
+        pluses = output[output.index("плюсы")+7:]
+        if "минусы" in output:
+            pluses = pluses[:pluses.index("минусы")]
+            pluses = pluses.split("\n")
+            pluses = [x[3:] for i, x in enumerate(pluses) if len(x)>0 and str(i+1) in x]
+        else:
+            minuses = []
+    else:
+        pluses = []
+            
+    if "минусы" in output:
+        minuses = output[output.index("минусы")+9:]
+        minuses = minuses.split("\n")
+        minuses = [x[3:] for i, x in enumerate(minuses) if len(x)>0 and str(i+1) in x]
+    else:
+        minuses = []
     
     return {"pluses": pluses, "minuses": minuses}
